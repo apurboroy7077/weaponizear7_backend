de866d75c883d22bea682bc41aaee128853ab6eb
Apurbo Roy <apurboroy7077@gmail.com>
2023-12-22 21:13:46 +0600

    Version 1, Environment Variable Fixed

--- configs/Variables.js
+++ configs/Variables.js
@@ -1,5 +1,5 @@
+require("dotenv").config();
 let serverPort = process.env.PORT || 3001;
-let dbURL =
-  "mongodb+srv://ar7:12345@cluster0.1g8wuka.mongodb.net/weaponizear7?retryWrites=true&w=majority";
-let jwtSecretKey = "WeaponizeAR7$Hmm";
+let dbURL = process.env.DATABASE_URL;
+let jwtSecretKey = process.env.JWT_SECRET_KEY;
 module.exports = { serverPort, dbURL, jwtSecretKey };
